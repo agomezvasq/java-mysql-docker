fbebd597c17d738f4b0b5fdd60218f2333f951d8
Santiago Vanegas <svaneg11@gmail.com>
2018-04-01 03:18:08 -0500

    commit

--- src/Main.java
+++ src/Main.java
@@ -50,9 +50,15 @@ public class Main {
 			System.out.println("Inserte argumentos");
 			return;
 		}
+		
+		CarreraController carreraController = new CarreraController();
+		EstudianteController estudianteController = new EstudianteController();
+		ICarreraDAO carreraDAO = new CarreraDAO();
+		IEstudianteDAO estudianteDAO = new EstudianteDAO();
+		
 		if(args[0].equalsIgnoreCase("INSERT")) {
 			if(args[1].equalsIgnoreCase("carrera")) {
-				ICarreraDAO carreraDAO = new CarreraDAO();
+				
 				List<Carrera> carreras = carreraDAO.getAll();
 				for (Carrera c:carreras) {
 					if(c.getNombre().equalsIgnoreCase(args[2])) {
@@ -62,15 +68,14 @@ public class Main {
 					
 				}
 				Carrera carrera = new Carrera(args[2]);
-				CarreraController carreraController = new CarreraController();
 				
 				carreraController.register(carrera);
 				carreras = carreraDAO.getAll();
 				carrera.setIdCarrera(carreras.size());
-				System.out.println("Registros actualizados.");
+				System.out.println("Registro de carreras actualizado.");
 				
 			}else if(args[1].equalsIgnoreCase("estudiante")) {
-				IEstudianteDAO estudianteDAO = new EstudianteDAO();
+				
 				List<Estudiante> estudiantes = estudianteDAO.getAll();
 				for (Estudiante e:estudiantes) {
 					if(e.getDocumento().equalsIgnoreCase(args[3])) {
@@ -78,26 +83,65 @@ public class Main {
 						return;
 					}
 				}
+				try{
 				Estudiante estudiante = new Estudiante(args[2],
 						args[3],
 						args[4],
 						args[5],
 						args[6],
 						args[7],
-						   carrera); // en lugar de carrera, Buscar una carrera ya creada usando un argumento idCarrera
+						   carreraController.get(Integer.parseInt(args[8])));
+				estudianteController.register(estudiante);
+				estudiantes = estudianteDAO.getAll();
+				estudiante.setIdEstudiante(estudiantes.size());
+				
+				}catch(Exception e) {
+					System.out.println("Número de argumentos incorrecto o la carrera especificada no existe.");
+					return;
+				}
+				System.out.println("Registros de estudiantes actualizado.");
+			}else {
+				System.out.println("Argumentos inválidos");
 			}
 		}
 		if(args[0].equalsIgnoreCase("READ")) {
+			if(args[1].equalsIgnoreCase("carreras")){
+				printCarreras(carreraDAO.getAll());
+			}else if(args[1].equalsIgnoreCase("estudiantes")) {
+				printEstudiantes(estudianteDAO.getAll());
+			}else {
+				System.out.println("Sintaxis: READ <Tabla>  Ejemplo: Read carreras, Read estudiantes");
+			}
 			
 		}
 		if(args[0].equalsIgnoreCase("UPDATE")) {
-			
+		
 		}
 		if(args[0].equalsIgnoreCase("DELETE")) {
-			
+			if(args[1].equalsIgnoreCase("carreras")) {
+				carreraDAO.delete(carreraDAO.get(Integer.parseInt(args[2])));
+				System.out.println("Registro "+args[2]+"de la tabla carreras eliminado.");
+			}else if(args[1].equalsIgnoreCase("estudiantes")) {
+				estudianteDAO.delete(estudianteDAO.get(Integer.parseInt(args[2])));
+				System.out.println("Registro "+args[2]+"de la tabla estudiantes eliminado.");
+			}else {
+				System.out.println("Sintaxis: Delete <Tabla> <id>  Ejemplo: Delete carreras 4, Delete estudiantes 2");
+			}
 		}
 		
 		System.out.println(args[0]);
 		
+	}
+	public static void printCarreras(List<Carrera> list){
+		for(Object c:list) {
+			c.toString();
+			System.out.println("");
+		}
+	}
+	public static void printEstudiantes(List<Estudiante> list){
+		for(Object e:list) {
+			e.toString();
+			System.out.println("");
+		}
 	}
 }
